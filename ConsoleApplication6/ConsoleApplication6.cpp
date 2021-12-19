0fc3f028cece942a34e0ca82fd0e67d69c3abeb0
Ziky-cell <53793259+Ziky-cell@users.noreply.github.com>
2021-12-19 16:29:13 +0300

    Update ConsoleApplication6.cpp

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -1,31 +1,18 @@
-﻿#include "stdafx.h"//дан массив A из N элементов, состоящий из нулей и еди - ниц.заменить единицы нулями и наоборот.
-#include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <clocale>
-#include<cstring>
-#define A 10
-#define B 50
-using namespace std;
-int main()
-{
-    setlocale(LC_ALL, "Russian");
-    char str[A][B], s[B], s1[B];
-    int i, j, a, b, ind, len, k;
-    char* start, * end, t;
-    cout << "Введите кол-во строк, которое вы собираетесь написать: ";
-    do
-    {
-        (cin >> k).get();
-        if (k < A) break;
-        else cout << "Кол-во строк не должно превышать 10-ти. Повторите попытку\n";
-    } while (k > A);
-
-
-    cout << "Введите строки(кол-во символов должно быть меньше 50):\n";
-    for (i = 0; i < k; i++)
-    {
-        gets_s(str[i]);
+#include <iostream>//дан массив A из N элементов, состоящий из нулей и еди - ниц.заменить единицы нулями и наоборот.
+#include <string>
+ 
+int main() {
+    using namespace std;
+    string text;
+   cout << "введите числа единицы и нули: " ;
+    getline(cin, text);
+   for (size_t i = 0; i < text.length(); i++) {
+        if (text[i] == '1') {
+            text[i] = '0';
+        } else if (text[i] == '0') {
+            text[i] = '1';
+          }
     }
-
-    cout << "Вы ввели:" << endl; 
+  cout << "значение:  ";
+    cout << text << endl;
+  }
